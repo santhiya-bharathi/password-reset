1ad0765a490d8e7b59e474f5b2f092eb8145b2a4
santhiya-bharathi <santhiyabharathi1999@gmail.com>
2021-12-20 21:27:38 +0530

    login page created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import {useState} from "react";
+import Button from '@mui/material/Button';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { Switch, Route } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import TextField from '@mui/material/TextField';
+
+export default function App() {
+  const [mode, setMode] = useState("dark");
+const darkTheme = createTheme({
+  palette: {
+    mode: mode,
+  },
+});
+const history = useHistory();
   return (
+    <ThemeProvider theme={darkTheme}>
+    <Paper elevation={3} style={{borderRadius:"0px",minHeight:"100vh"}}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <AppBar position="static">
+       <Toolbar>
+       <Button varient="text" color="inherit"  onClick={()=>history.push("/login")}> log in </Button>
+      <Button varient="text" color="inherit"  onClick={()=>setMode(mode==="light"? "dark":"light")}> {mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />} {mode==="light"? "Dark":"Light"}Mode</Button>
+  
+      </Toolbar>
+       </AppBar>
+       
+       <Switch>
+     
+
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+
+        </Switch>
+    </div>
+    </Paper>
+    </ThemeProvider>
+  );
+}
+
+function LoginPage(){
+  return(
+    <div className="login-page">
+     <h1 className="login-head">Login</h1>
+     <h4 className="please">Please enter your e-mail id and Password</h4>
+     <TextField id="outlined-basic" label="e-mail id" variant="outlined" />
+     <TextField
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+        />
+        <Button variant="outlined">log in</Button>
+       
     </div>
   );
 }
 
-export default App;
+
